58e760fce00b5a3086e266ec5abe098a1dbf564e
sergioosoriozapata <sergioosoriozapata@gmail.com>
2026-07-22 16:28:45 -0500

    Primer commit

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+export default function App() {
+const fecha = new Date().toLocaleString();
+return (
+ <main>
+ <h1>Hola SENA Soy Sergio</h1>
+ <p>Soy Aprendiz SENA De Desarrollo y Analisis de software y es mi primera vez en node.js</p>
+ </main>)}
