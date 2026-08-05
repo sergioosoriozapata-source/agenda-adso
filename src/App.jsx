e8d0c5314f21b42188196179f86a99a25784c36e
sergioosoriozapata <sergioosoriozapata@gmail.com>
2026-08-05 15:45:27 -0500

    Actividad,clase 3 componentes y props

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,67 @@
+import ContactoCard from "./components/ContactoCard";
+import "./App.css"
+
 export default function App() {
-const fecha = new Date().toLocaleString();
-return (
- <main>
- <h1>Hola SENA Soy Sergio</h1>
- <p>Soy Aprendiz SENA De Desarrollo y Analisis de software y es mi primera vez en node.js</p>
- </main>)}
+  // Esta es nuestra "base de datos" inicial quemada en el código
+  const contactos = [
+    {
+      id: 1,
+      nombre: "Carolina Pérez",
+      telefono: "300 123 4567",
+      correo: "carolina@sena.edu.co",
+      etiqueta: "Compañera",
+    },
+    {
+      id: 2,
+      nombre: "Juan Díaz",
+      telefono: "301 987 6543",
+      correo: "juan@sena.edu.co",
+      etiqueta: "Instructor",
+    },
+    {
+      id: 3,
+      nombre: "Luisa Martínez",
+      telefono: "320 555 7788",
+      correo: "luisa@sena.edu.co",
+      etiqueta: "Cliente",
+    },
+    {
+      id: 4,
+      nombre: "Saul andres ",
+      telefono: "333 2829596",
+      correo: "Saul@sena.edu.co",
+      etiqueta: "compañero",
+    },
+    {
+      id: 5,
+      nombre: "Carlos",
+      telefono: "312 3123234",
+      correo: "Carlos@sena.edu.co",
+      etiqueta: "compañero",
+    },
+  ];
+
+  return (
+    <main className="app-container">
+      <h1 className="app-title">Agenda ADSO 📒</h1>
+
+      <p className="app-subtitle">Contactos guardados</p>
+
+      {/* Recorremos el arreglo contactos y pintamos una tarjeta por cada uno */}
+      {contactos.map((c) => (
+        <ContactoCard
+          key={c.id}            // key única para React
+          nombre={c.nombre}     // prop nombre
+          telefono={c.telefono} // prop telefono
+          correo={c.correo}     // prop correo
+          etiqueta={c.etiqueta} // prop etiqueta (Cliente, Instructor, etc.)
+        />
+      ))}
+
+      <p className="app-nota">
+        (Versión 0.1 - solo lectura, sin agregar ni editar todavía)
+      </p>
+    </main>
+  );
+}
+
