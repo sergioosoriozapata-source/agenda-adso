0103bf15945e1d9b82a95b5b153aa297fcb94417
sergioosoriozapata <sergioosoriozapata@gmail.com>
2026-08-19 17:10:44 -0500

    Clase_6_Tailwind_UI_Agenda_ADSO_v3

--- src/components/ContactoCard.jsx
+++ src/components/ContactoCard.jsx
@@ -1,12 +1,19 @@
 export default function ContactoCard({ nombre, telefono, correo, etiqueta, onEliminar }) {
   return (
-    <article className="tarjeta-contacto">
-      <h3>{nombre}</h3>
-      <p>📞 {telefono}</p>
-      <p>✉️ {correo}</p>
+    <article className="bg-white border border-gray-200 rounded-xl shadow-sm p-5">
+      <h3 className="text-xl font-bold text-gray-900 mb-3">
+        {nombre}
+        </h3>
+      <p className="text-gray-700 mb-1">
+        📞 {telefono}
+        </p>
+      <p className="text-gray-700 mb-1">
+        ✉️ {correo}
+        </p>
       {etiqueta && <p>{etiqueta}</p>}
-      <div className="acciones">
-        <button className="btn-eliminar" onClick={() => onEliminar(correo)}>
+      <div className="flex justify-start" >
+        <button className="bg-red-500 hover:bg-red-600 text-white text-sm font-medium 
+px-4 py-2 rounded-lg transition-colors" onClick={() => onEliminar(correo)}>
           Eliminar
         </button>
       </div>
