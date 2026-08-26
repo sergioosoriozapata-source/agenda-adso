070783e8b8d04015dd3dee42efbf034c3c1a23eb
sergioosoriozapata <sergioosoriozapata@gmail.com>
2026-08-26 14:01:01 -0500

    Conexion con apis y el CRUD

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+// Importamos el componente raíz
+import App from "./App.jsx";
+// Importante: trae Tailwind a la app
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
- <React.StrictMode>
-  <App />
- </React.StrictMode>
+// Punto de entrada de la app: renderiza <App /> dentro de #root
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
